3e18308e023258812f0a078b00644d268078b23a
Berenice Soto <beresoto.f@gmail.com>
2021-03-21 18:50:52 -0600

    first commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer
+  >
+    <div>
+      <h1>soy un fooder</h1>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TheFooter',
+})
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1 class="container">{{mensaje}}</h1>
+    <the-footer />
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios';
+import env from '../config/env';
+import TheFooter from '~/components/TheFooter.vue'
+export default Vue.extend({
+    components: {
+        TheFooter,
+        },
+        data(){
+        return{
+            mensaje:'holas'
+        }
+    },
+    created(){
+        axios.get(`${env.endpoint}/albums`).then(response =>{
+            console.log(response);
+        })
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
